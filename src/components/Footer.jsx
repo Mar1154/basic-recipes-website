b5ddea9141b618b4a92316bfd62cb054808c3ad3
Mar1154 <noiramyeliab@gmail.com>
2025-01-27 02:26:23 +0800

    Fixed footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,15 @@ export function Footer() {
         <footer>
             <p>© 2024 Marion Bailey</p>
             <div className="footer-icons">
-                <a href=""><i className="fab fa-github"></i></a>
-                <a href=""><i className="fab fa-linkedin"></i></a>
-                <a href=""><i className="fab fa-discord"></i></a>
+                <a href="https://github.com/Mar1154">
+                    <i className="fab fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/marion-bailey-0803ba345/">
+                    <i className="fab fa-linkedin"></i>
+                </a>
+                <a href="https://www.upwork.com/freelancers/~01393b34980572dd70">
+                    <i className="custom-upwork-icon"></i>
+                </a>
             </div>
         </footer>
     )
